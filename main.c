5bd4db11d32c57db54eabeb5a9175bc3db658db2
j4cobgarby <thyself@jacobgarby.co.uk>
2018-10-08 12:38:22 +0100

    challenge 1

--- main.c
+++ main.c
@@ -6,14 +6,14 @@ int has_correct_digits(int n) {
         do {
                 tmp = n % 10;
                 if (tmp < 1 || tmp > 3) return 0;
-                tmp /= 10;
+                n /= 10;
         } while (n);
 
         return 1;
 }
 
 int main() {
-        int amount;
+        int amount = 0;
 
         for (int i = 10000008; i < 100000000; i += 18) {
                 if (has_correct_digits(i)) amount++;
